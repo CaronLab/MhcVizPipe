657e95777becb712c4cfb57e0782011c1d85563e
kevin <kkovalchik.chusj@gmail.com>
2020-08-09 16:38:06 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='MhcQcPipe',
+    name='MhcVizPipe',
     version='',
-    packages=['MhcQcPipe', 'MhcQcPipe.Tools', 'MhcQcPipe.ReportTemplates'],
-    url='',
-    license='',
+    packages=find_packages(),
+    url='https://github.com/kevinkovalchik/MhcVizPipe',
+    license='MIT',
     author='Kevin Kovalchik',
     author_email='',
-    description=''
+    description='A reporting pipeline for visualization of immunopeptidomics MS data.',
+    python_requires='>=3.7',
+    install_requires=[
+        'dash>=1.12.0',
+        'plotly',
+        'dash-bootstrap-components',
+        'pandas',
+        'numpy',
+        'dominate',
+        'upsetplot',
+        'seaborn',
+        'plotly-logo',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
