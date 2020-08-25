68d715b5c0fa5658aa6c786872878a072121100f
kevin <kkovalchik.chusj@gmail.com>
2020-08-25 13:46:45 -0400

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='MhcVizPipe',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     url='https://github.com/kevinkovalchik/MhcVizPipe',
     license='MIT',
