9ead4b8819c44257cb98e490331c7f71c54fb027
kevin <kkovalchik.chusj@gmail.com>
2020-08-28 14:00:02 -0400

    update version

--- MhcVizPipe/gui.py
+++ MhcVizPipe/gui.py
@@ -960,7 +960,7 @@ def get_report(path):
 if __name__ == '__main__':
     welcome = f'''
      ========================================
-     MhcVizPipe v0.3.0
+     MhcVizPipe v0.3.1
 
      Welcome to MhcVizPipe! To open the GUI, open the following link
      in your web browser: http://{Parameters.HOSTNAME}:{Parameters.PORT}
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='MhcVizPipe',
-    version='0.3.0',
+    version='0.3.1',
     packages=find_packages(),
     url='https://github.com/kevinkovalchik/MhcVizPipe',
     license='MIT',
