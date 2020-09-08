388af69cd2e48f408bc732888c20fa0eb51b7b74
kevin <kkovalchik.chusj@gmail.com>
2020-09-08 16:03:49 -0400

    update version

--- MhcVizPipe/gui.py
+++ MhcVizPipe/gui.py
@@ -997,7 +997,7 @@ def get_report(path):
 if __name__ == '__main__':
     welcome = f'''
      ========================================
-     MhcVizPipe v0.4.2
+     MhcVizPipe v0.4.3
 
      Welcome to MhcVizPipe! To open the GUI, open the following link
      in your web browser: http://{Parameters.HOSTNAME}:{Parameters.PORT}
