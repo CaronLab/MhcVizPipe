20fb97e0ca51ea54bcbdd1a9abf44ff26023e84e
kevin <kkovalchik.chusj@gmail.com>
2020-09-11 13:14:50 -0400

    update version

--- MhcVizPipe/gui.py
+++ MhcVizPipe/gui.py
@@ -1135,7 +1135,7 @@ def get_report(path):
 if __name__ == '__main__':
     welcome = f'''
      ========================================
-     MhcVizPipe v0.4.4
+     MhcVizPipe v0.4.5
 
      Welcome to MhcVizPipe! To open the GUI, open the following link
      in your web browser: http://{Parameters.HOSTNAME}:{Parameters.PORT}
@@ -1161,7 +1161,7 @@ if __name__ == '__main__':
     You are running MhcVizPipe in debugging mode. This uses the
     development server shipped with Flask.
     
-    The GUI running here: http://{Parameters.HOSTNAME}:8971
+    The GUI is running here: http://{Parameters.HOSTNAME}:8971
 
     ========================================
     '''
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='MhcVizPipe',
-    version='0.4.4',
+    version='0.4.5',
     packages=find_packages(),
     url='https://github.com/CaronLab/MhcVizPipe',
     license='MIT',
