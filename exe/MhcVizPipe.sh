6fe3dda9d5433db124dc34418e9ee6b1d4a26ad9
kevin <kkovalchik.chusj@gmail.com>
2021-04-28 08:33:29 -0400

    fix logic in search for tool scripts

--- exe/MhcVizPipe.sh
+++ exe/MhcVizPipe.sh
@@ -7,29 +7,37 @@ CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 if [[ ! "$CDIR" == *MhcVizPipe ]]; then
 	echo "ERROR: It looks like the MhcVizPipe executable has been moved out of its installation directory or the \
 installation directory has been renamed. The executable needs to remain inside the installation directory and the \
-installation directory must not be renamed (i.e. it should still be called MhcVizPipe)."
+installation directory must not be renamed (i.e. it should still be called MhcVizPipe).";
+  read -rsp "Press any key to exit..." -n1 key;
+  printf "\n";
 	exit 1
 fi
 
 if [[ ! -e "$CDIR"/tools ]]; then
 	echo "ERROR: The \"tools\" folder is missing from the MhcVizPipe folder. If you have moved it, please move it back to \
 its original location. If it has been deleted or is missing, you will need to reinstall MhcVizPipe (or \
-replace the tool folder by extracting it from the MhcVizPipe download)."
+replace the tool folder by extracting it from the MhcVizPipe download).";
+  read -rsp "Press any key to exit..." -n1 key;
+  printf "\n";
 	exit 1
 fi
 
 if [[ ! -e "$CDIR"/python ]]; then
 	echo "ERROR: The \"python\" folder is missing from the MhcVizPipe folder. If you have moved it, please move it back to \
 its original location. If it has been deleted or is missing, you will need to reinstall MhcVizPipe (or replace the python \
-folder by extracting it from the MhcVizPipe download)."
+folder by extracting it from the MhcVizPipe download).";
+  read -rsp "Press any key to exit..." -n1 key;
+  printf "\n";
 	exit 1
 fi
 
-if [[ ! (-e "$CDIR"/tools/gibbscluster && -e "$CDIR"/tools/netMHCIIpan) && -e "$CDIR"/tools/netMHCpan4.0 && -e "$CDIR"/tools/netMHCpan4.1 ]]; then
+if [[ ! (-e "$CDIR"/tools/gibbscluster && -e "$CDIR"/tools/netMHCIIpan && -e "$CDIR"/tools/netMHCpan4.0 && -e "$CDIR"/tools/netMHCpan4.1) ]]; then
 	echo "One or more of the tool scripts are missing from the \"tools\" folder. It should have the following scipt files: \
 netMHCpan4.0, netMHCpan4.1, netMHCIIpan, and gibbscluster. Please replace these files either by downloading them from \
 https://github.com/CaronLab/MhcVizPipe/tree/master/tool_scripts or extracting them from your original MhcVizPipe download \
-(if you still have it)."
+(if you still have it).";
+  read -rsp "Press any key to exit..." -n1 key;
+  printf "\n";
 	exit 1
 fi
 
