30148d5cb06d88a31f348e19c96f473fafa8acef
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-19 13:01:47 +0000

    Adding tarfile member sanitization to extractall()

--- MhcVizPipe/gui.py
+++ MhcVizPipe/gui.py
@@ -1200,7 +1200,29 @@ def download_data_file(tool: str) -> None:
     print('\nExtracting archive... ', end="", flush=True)
     try:
         with tarfile.open('./data.tar.gz', 'r:gz') as tar:
-            tar.extractall()
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar)
     except Exception:
         print(f'\nERROR: Sorry! There was a problem extracting the downloaded data file for {tool}. This might be due to '
               f'a corrupted download. Please try restarting the program and if the problem persists download the data '
